7e47e0770c6ad53e15d6b8314d3a3d2e42c67561
MrMonkek <GregersRisbjerg@gmail.com>
2025-05-27 15:03:47 +0200

    Player Movement og Camera Movement
    tilføjelse af bevægelse og kamera der følger spiller

---
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class IsometricCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(-10f, 15f, -10f);
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
