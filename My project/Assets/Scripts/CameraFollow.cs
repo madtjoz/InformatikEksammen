ceb55aac18058832ed94fc633e8c63badee5b4a1
MrMonkek <GregersRisbjerg@gmail.com>
2025-05-27 15:19:55 +0200

    Fix script
    Script fix til player

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class IsometricCameraFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset = new Vector3(-10f, 15f, -10f);
